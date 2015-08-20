08ca20b5ddd21a09869c42c063a2ad8e0af5596a
Samiul Hoque <samiul@zetapps.com>
2015-08-21 00:52:36 +0600

    Lesson 3 MinAvgTwoSlice solution

---
+++ src/Lesson03/MinAvgTwoSlice.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Lesson03;
+
+class MinAvgTwoSlice
+{
+
+    function solution($A)
+    {
+        $position = 0;
+        $count = count($A);
+        $minAvg = ($A[0] + $A[1]) / 2;
+
+        for ($p = 0; $p < $count - 1; $p++) {
+            $avg = ($A[$p] + $A[$p + 1]) / 2;
+            if ($avg < $minAvg) {
+                $minAvg = $avg;
+                $position = $p;
+            }
+
+            if (isset($A[$p + 2])) {
+                $avg = ($A[$p] + $A[$p + 1] + $A[$p + 2]) / 3;
+                if ($avg < $minAvg) {
+                    $minAvg = $avg;
+                    $position = $p;
+                }
+            }
+        }
+
+        return $position;
+    }
+}
