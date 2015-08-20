dd3f9faa74378ae7f49ad3d461f9f1118a84d25d
Samiul Hoque <samiul@zetapps.com>
2015-08-20 23:55:27 +0600

    Lesson 3 PassingCars solution

---
+++ spec/Lesson03/PassingCarsSpec.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace spec\Lesson03;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class PassingCarsSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Lesson03\PassingCars');
+    }
+
+    function it_should_return_5_for_01011()
+    {
+        $this->solution([0, 1, 0, 1, 1])->shouldReturn(5);
+    }
+
+    function it_should_return_5_for_010110()
+    {
+        $this->solution([0, 1, 0, 1, 1, 0])->shouldReturn(5);
+    }
+
+    function it_should_return_8_for_0101101()
+    {
+        $this->solution([0, 1, 0, 1, 1, 0, 1])->shouldReturn(8);
+    }
+}
---
+++ src/Lesson03/PassingCars.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Lesson03;
+
+class PassingCars
+{
+
+    function solution($A)
+    {
+        $count = count($A);
+        $sum = 0;
+        $total = 0;
+        for ($i = $count - 1; $i >= 0; $i--) {
+            if ($A[$i] == 1) {
+                $sum++;
+            } else {
+                $total += $sum;
+            }
+
+            if ($total > 1000000000) {
+                $total = -1;
+                break;
+            }
+        }
+        return $total;
+    }
+}
