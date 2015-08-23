5fab407105de7930cd2abf65c252f4e45eaaac44
Samiul Hoque <samiul@zetapps.com>
2015-08-23 06:41:28 +0600

    Lesson 05 Fish solution

--- src/Lesson05/Fish.php
+++ src/Lesson05/Fish.php
@@ -4,4 +4,31 @@ namespace Lesson05;
 
 class Fish
 {
+
+    function solution($A, $B)
+    {
+        $N = count($A);
+        $stack = [];
+        for ($i = 0; $i < $N; $i++) {
+            $size = $A[$i];
+            $direction = $B[$i];
+            if ($i == 0) {
+                array_push($stack, [$size, $direction]);
+            } else {
+                $v = array_pop($stack);
+                while ($v[1] > $direction) {
+                    if ($v[0] > $size) {
+                        $size = $v[0];
+                        $direction = $v[1];
+                    }
+                    $v = array_pop($stack);
+                }
+                if ($v != null && $v[0] !== $size) {
+                    array_push($stack, [$v[0], $v[1]]);
+                }
+                array_push($stack, [$size, $direction]);
+            }
+        }
+        return count($stack);
+    }
 }
