4eae1c9f69f512dc832331757a60c2c87b176361
Samiul Hoque <samiul@zetapps.com>
2015-08-24 06:11:05 +0600

    Lesson 8 MinPerimeterRectangle solution

--- src/Lesson08/MinPerimeterRectangle.php
+++ src/Lesson08/MinPerimeterRectangle.php
@@ -20,17 +20,3 @@ class MinPerimeterRectangle
         return 2 * $min;
     }
 }
-function solution($N)
-{
-    $root = (int)ceil(sqrt($N));
-    $min = PHP_INT_MAX;
-    for ($i = 1; $i <= $root; $i++) {
-        if ($N % $i === 0) {
-            $j = $N / $i;
-            if ($i + $j < $min) {
-                $min = $i + $j;
-            }
-        }
-    }
-    return 2 * $min;
-}
