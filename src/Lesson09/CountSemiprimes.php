8ed4d135a7d47ec0337a435eadcf483c8dd131de
Samiul Hoque <samiul7283@gmail.com>
2015-09-07 03:51:14 +0600

    Updated for codeclimate - CountSemiprimes

--- src/Lesson09/CountSemiprimes.php
+++ src/Lesson09/CountSemiprimes.php
@@ -8,22 +8,10 @@ class CountSemiprimes
     {
         $initialArrayCount = count($P);
         $primes = array_values($this->filterPrimes($N, $this->getPrimes($N)));
-
-        $semiPrimes = [];
         $primeCount = count($primes);
-
-        for ($i = 0; $i < $primeCount; $i++) {
-            for ($j = $i; $j < $primeCount; $j++) {
-                $primesProduct = $primes[$i] * $primes[$j];
-                if ($primesProduct <= $N) {
-                    $semiPrimes[$primesProduct] = $primesProduct;
-                } else {
-                    break;
-                }
-            }
-        }
-
+        $semiPrimes = $this->getSemiPrimes($N, $primeCount, $primes);
         ksort($semiPrimes);
+
         $assignableValue = 1;
         foreach ($semiPrimes as $value) {
             $semiPrimes[$value] = $assignableValue;
@@ -33,33 +21,12 @@ class CountSemiprimes
         $semiPrimesInRange = [];
 
         for ($i = 0; $i < $initialArrayCount; $i++) {
-            $leftKey = null;
-            $rightKey = null;
             $pValue = $P[$i];
             $qValue = $Q[$i];
             $range = $qValue - $pValue;
 
-            if (array_key_exists($pValue, $semiPrimes)) {
-                $leftKey = $pValue;
-            } else {
-                for ($j = 1; $j <= $range; $j++) {
-                    if (array_key_exists($pValue + $j, $semiPrimes)) {
-                        $leftKey = $pValue + $j;
-                        break;
-                    }
-                }
-            }
-
-            if (array_key_exists($qValue, $semiPrimes)) {
-                $rightKey = $qValue;
-            } else {
-                for ($j = 1; $j <= $range; $j++) {
-                    if (array_key_exists($qValue - $j, $semiPrimes)) {
-                        $rightKey = $qValue - $j;
-                        break;
-                    }
-                }
-            }
+            $leftKey = $this->getKey($pValue, $semiPrimes, $range);
+            $rightKey = $this->getKey($qValue, $semiPrimes, $range);
 
             if ($leftKey === null || $rightKey === null) {
                 $semiPrimesInRange[] = 0;
@@ -100,5 +67,50 @@ class CountSemiprimes
             $initialValue++;
         }
         return $primes;
+    }
+
+    /**
+     * @param $N
+     * @param $primeCount
+     * @param $primes
+     * @return array
+     * @internal param $semiPrimes
+     */
+    private function getSemiPrimes($N, $primeCount, $primes)
+    {
+        $semiPrimes = [];
+        for ($i = 0; $i < $primeCount; $i++) {
+            for ($j = $i; $j < $primeCount; $j++) {
+                $primesProduct = $primes[$i] * $primes[$j];
+                if ($primesProduct <= $N) {
+                    $semiPrimes[$primesProduct] = $primesProduct;
+                } else {
+                    break;
+                }
+            }
+        }
+        return $semiPrimes;
+    }
+
+    /**
+     * @param $value
+     * @param $semiPrimes
+     * @param $range
+     * @return key
+     */
+    private function getKey($value, $semiPrimes, $range)
+    {
+        $key = null;
+        if (array_key_exists($value, $semiPrimes)) {
+            $key = $value;
+        } else {
+            for ($j = 1; $j <= $range; $j++) {
+                if (array_key_exists($value + $j, $semiPrimes)) {
+                    $key = $value + $j;
+                    break;
+                }
+            }
+        }
+        return $key;
     }
 }
