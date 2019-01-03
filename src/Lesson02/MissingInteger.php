42967001deabd94203c8faacfb721533f860cde5
shyamKumarPariyar <aps.sami2013@gmail.com>
2019-01-04 00:57:03 +0545

    Update MissingInteger.php

--- src/Lesson02/MissingInteger.php
+++ src/Lesson02/MissingInteger.php
@@ -4,35 +4,14 @@ namespace Lesson02;
 
 class MissingInteger
 {
-    public function solution($A)
-    {
-        $uniquePositives = array_unique(array_filter($A, function ($number) {
-            return $number > 0;
-        }));
-
+   function solution($A) {
+        sort($A);
         $missingNumber = 1;
-        $count = count($uniquePositives);
-
-        if ($count > 0) {
-            if ($count > 1) {
-                asort($uniquePositives);
-            }
-
-            $sortedUniques = array_values($uniquePositives);
-            $sortedUniquesCount = count($sortedUniques);
-
-            for ($i = 0; $i < $sortedUniquesCount; $i++) {
-                if ($sortedUniques[$i] > ($i + 1)) {
-                    $missingNumber = $i + 1;
-                    break;
-                }
-            }
-
-            if ($i == $sortedUniquesCount) {
-                $missingNumber = $i + 1;
-            }
+        foreach($A as $value)
+        {
+            if($value == $missingNumber) 
+                $missingNumber++;
         }
-
         return $missingNumber;
     }
 }
